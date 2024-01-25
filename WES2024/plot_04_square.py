0fbc7fdbe11ce159bc340952f78bee81e2e766fe
Jaime Liew <jaimeliew1@gmail.com>
2024-01-25 16:58:45 -0500

    Add square layout wind farm optimisation

---
+++ WES2024/plot_04_square.py
@@ -0,0 +1,156 @@
+from itertools import product
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import polars as pl
+from cache import cache_polars
+from foreach import foreach
+from mitwindfarm import Plotting
+from mitwindfarm.Layout import Square
+from mitwindfarm.windfarm import Windfarm
+from optimise import JointControl, NoControl, ThrustControl, YawControl
+from utilities import from_polars, to_polars
+
+
+# Use Latex Fonts
+plt.rcParams.update({"text.usetex": True, "font.family": "serif"})
+
+
+FIGDIR = Path(__file__).parent.parent / "fig"
+FIGDIR.mkdir(exist_ok=True, parents=True)
+
+windfarm = Windfarm()
+
+
+methods = {
+    "NoControl": NoControl,
+    "YawControl": YawControl,
+    "ThrustControl": ThrustControl,
+    "JointControl": JointControl,
+}
+
+plot_params = {
+    "NoControl": dict(ls="--", c="k", label="No Control"),
+    "ThrustControl": dict(c="tab:blue", label="Thrust Control"),
+    "YawControl": dict(c="tab:orange", label="Yaw Control"),
+    "JointControl": dict(c="tab:green", label="Joint Control"),
+}
+
+layouts = {
+    4: Square(4.0, 5),
+    6: Square(6.0, 5),
+    8: Square(8.0, 5),
+    10: Square(10.0, 5),
+}
+wdirs = np.arange(0, 360, 1)
+
+
+# @profile(filename="prof.prof")
+def _generate(x):
+    method, wdir, min_dist = x
+    sol = methods[method](layouts[min_dist].rotate(wdir), windfarm).optimise(use_gradients=True)
+    return to_polars(sol).with_columns(
+        pl.lit(method).alias("method"),
+        pl.lit(wdir).alias("wdir"),
+        pl.lit(min_dist).alias("min_dist"),
+    )
+
+
+@cache_polars(Path(__file__).parent.parent / "data/plot_04_square.csv")
+def generate(regenerate=False):
+    params = list(product(methods, wdirs, layouts))
+
+    df = pl.concat(foreach(_generate, params, parallel=True))
+    return df
+
+
+def plot_Cp_vs_distance(df: pl.DataFrame):
+    N_dist = df["min_dist"].n_unique()
+
+    fig, axes = plt.subplots(N_dist, 1, sharex=True, sharey=True)
+
+    for ax, min_dist in zip(axes, df["min_dist"].unique().sort()):
+        ax.text(0.5, 0.99, f"turbine spacing: {min_dist}D", ha="center", va="top", transform=ax.transAxes)
+        for method, _plot_params in plot_params.items():
+            _df = df.filter(pl.col("min_dist") == min_dist).filter(pl.col("method") == method)
+            to_plot = _df.group_by("wdir").agg(pl.col("Cp").mean()).sort("wdir")
+            ax.plot(to_plot["wdir"], to_plot["Cp"], **plot_params[method])
+
+    axes[-1].set_xlabel("wind direction (deg)")
+    [ax.set_ylabel("$C_P$") for ax in axes]
+
+    axes[0].set_ylim(0.1, 0.6)
+
+    axes[0].legend(loc="lower center", bbox_to_anchor=(0.5, 1.01), ncol=4)
+
+    axes[0].set_xlim(0, 360)
+    plt.savefig(FIGDIR / "square_Cp_vs_distance.png", dpi=300, bbox_inches="tight")
+
+
+def plot_farm_performance_vs_distance(df: pl.DataFrame):
+    df_farm_Cp = df.pivot(columns="method", index="min_dist", values="Cp", aggregate_function="mean")
+    df_farm_Cp = df_farm_Cp.select(
+        pl.col("min_dist"),
+        pl.exclude("min_dist", "NoControl") / pl.col("NoControl") * 100 - 100,
+    )
+    print(df_farm_Cp)
+
+    methods = df_farm_Cp.columns
+    methods.remove("min_dist")
+
+    plt.figure()
+    for method, _plot_params in plot_params.items():
+        if method == "NoControl":
+            continue
+        plt.plot(df_farm_Cp["min_dist"], df_farm_Cp[method], **_plot_params)
+
+    plt.legend()
+
+    plt.xlabel("Turbine spacing [D]")
+    plt.ylabel(r"Power increase [\%]")
+    plt.savefig(FIGDIR / "square_farm_performance_vs_distance.png", dpi=300, bbox_inches="tight")
+
+
+def plot_windfarm(df: pl.DataFrame):
+    for min_dist in df["min_dist"].unique():
+        _df = (
+            df.filter(pl.col("method") == "JointControl")
+            .filter(pl.col("min_dist") == min_dist)
+            .filter(pl.col("wdir") == 0)
+        )
+        windfarm_sol = from_polars(_df, windfarm)
+
+        Plotting.plot_windfarm(windfarm_sol)
+        plt.savefig(FIGDIR / f"square_windfarm_{min_dist}D.png", dpi=300, bbox_inches="tight")
+        plt.close()
+
+
+def plot_setpoints(df: pl.DataFrame):
+    df = df.filter(pl.col("min_dist") == 4).filter(pl.col("method") == "JointControl")
+
+    fig, axes = plt.subplots(2, 1, sharex=True)
+
+    for i, _df in df.group_by("turbine"):
+        axes[0].plot(_df["wdir"], np.rad2deg(_df["yaw"]), c=plt.cm.gist_ncar(i / 20))
+        axes[1].plot(_df["wdir"], _df["Ctprime"], c=plt.cm.gist_ncar(i / 20))
+
+    axes[-1].set_xlabel("wind direction [deg]")
+
+    axes[0].set_ylabel("yaw setpoint [deg]")
+    axes[1].set_ylabel("$C_T'$ setpoint")
+
+    axes[0].set_xlim(0, 360)
+    plt.savefig(FIGDIR / "square_setpoints.png", dpi=300, bbox_inches="tight")
+
+
+def main():
+    df = generate(regenerate=False)
+    plot_setpoints(df)
+    plot_windfarm(df)
+    plot_farm_performance_vs_distance(df)
+    plot_Cp_vs_distance(df)
+
+
+if __name__ == "__main__":
+    main()
