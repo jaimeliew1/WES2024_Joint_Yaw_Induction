ab7046cc20f4256360455e09c8c1500aa5dbcad9
Jaime Liew <jaimeliew1@gmail.com>
2024-02-29 20:31:49 +0000

    add AWS run script

--- WES2024/Fig11_diamond_pitch_tsr_surface.py
+++ WES2024/Fig11_diamond_pitch_tsr_surface.py
@@ -40,7 +40,7 @@ PARALLEL = True
 
 windfarm = Windfarm(rotor_model=BEM(IEA15MW(), BEM_model=DualBEM))
 layout = Square(10.0, 5).rotate(45)
-wdirs = np.arange(0.0, 45.0, 0.05)
+wdirs = np.arange(0.0, 90.0, 0.05)
 
 
 methods = {
---
+++ run.sh
@@ -0,0 +1,2 @@
+poetry run ipython WES2024/Fig11_diamond_pitch_tsr_surface.py  &> log.txt
+sudo shutdown now -h
