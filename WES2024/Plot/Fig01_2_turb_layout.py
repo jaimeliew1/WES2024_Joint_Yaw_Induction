397f5e19eb26a9186bafd01b130c359ce82e7945
Jaime Liew <jaimeliew1@gmail.com>
2024-03-11 16:41:46 -0400

    transform 2-turbine layout coordinates

--- WES2024/Plot/Fig01_2_turb_layout.py
+++ WES2024/Plot/Fig01_2_turb_layout.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
 from mitwindfarm import Plotting
-from mitwindfarm.windfarm import Windfarm
+from mitwindfarm.windfarm import Windfarm, WindfarmSolution
 
 from WES2024 import utils
 from WES2024.Generate import two_turbine_AD
@@ -23,15 +23,65 @@ def generate(regenerate=False) -> pl.DataFrame:
     return df
 
 
+def rotated_meshgrid(x, y, angle_rad):
+    xmesh, ymesh = np.meshgrid(x, y)
+    x0, y0 = xmesh.mean(), ymesh.mean()
+    xmesh -= x0
+    ymesh -= y0
+    # Convert angle to radians
+    angle_radians = angle_rad
+
+    # Rotate the coordinates
+    x_rotated = xmesh * np.cos(angle_radians) - ymesh * np.sin(angle_radians)
+    y_rotated = xmesh * np.sin(angle_radians) + ymesh * np.cos(angle_radians)
+
+    return x_rotated + x0, y_rotated + y0, x0, y0
+
+
+def plot_windfarm(sol: WindfarmSolution, ax=None, pad=1, frame=True, axis=False, angle=0):
+    if ax is None:
+        _, ax = plt.subplots()
+
+    angle_rad = np.deg2rad(angle)
+
+    xlim = (np.min(sol.layout.x) - pad, np.max(sol.layout.x) + 5)
+    ylim = (np.min(sol.layout.y) - pad, np.max(sol.layout.y) + pad)
+
+    _x, _y = np.linspace(*xlim, 600), np.linspace(*ylim, 600)
+    xmesh, ymesh, x0, y0 = rotated_meshgrid(_x, _y, angle_rad)
+    wsp = sol.windfield.wsp(xmesh, ymesh, np.zeros_like(xmesh))
+    ax.imshow(wsp, extent=[*xlim, *ylim], vmin=-0.1, vmax=2.1, origin="lower", cmap="RdYlBu_r")
+
+    for (turb_x, turb_y, _), rotor in zip(sol.layout, sol.rotors):
+        # Draw turbine
+        R = 0.5
+        p = np.array([[0, 0], [+R, -R]])
+        rotmat = np.array(
+            [
+                [np.cos(rotor.yaw - angle_rad), -np.sin(rotor.yaw - angle_rad)],
+                [np.sin(rotor.yaw - angle_rad), np.cos(rotor.yaw - angle_rad)],
+            ]
+        )
+        _turb_x = (turb_x - x0) * np.cos(-angle_rad) - (turb_y - y0) * np.sin(-angle_rad)
+        _turb_y = (turb_x - x0) * np.sin(-angle_rad) + (turb_y - y0) * np.cos(-angle_rad)
+        points = rotmat @ p + np.array([[_turb_x + x0], [_turb_y + y0]])
+        # breakpoint()
+        # color_denom = 0.65
+        color = "k"  # plt.cm.plasma(rotor.Cp / color_denom)
+        ax.plot(points[0, :], points[1, :], lw=2, c=color)
+    ax.set(frame_on=frame)
+    if axis is False:
+        ax.set(xticks=[], yticks=[])
+
+
 def plot(df: pl.DataFrame):
     windfarm = Windfarm()
 
     _df = df.filter(pl.col("method") == "JointControl").filter(np.abs(pl.col("wdir") - 5) < 0.01)
-
+    wdir = _df["wdir"][0]
     plt.figure()
-
     windfarm_sol = utils.from_polars(_df, windfarm)
-    Plotting.plot_windfarm(windfarm_sol)
+    plot_windfarm(windfarm_sol, plt.gca(), angle=wdir, pad=2)
     plt.savefig(utils.FIGDIR / f"{FILESTEM}.png", dpi=300, bbox_inches="tight")
     plt.close()
 
