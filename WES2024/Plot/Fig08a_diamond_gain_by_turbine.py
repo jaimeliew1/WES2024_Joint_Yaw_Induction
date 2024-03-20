987d29e779af3c8dde40403dd30162a6009debbe
Jaime Liew <jaimeliew1@gmail.com>
2024-03-20 13:49:46 -0400

    add turbine Ct barplot

--- WES2024/Plot/Fig08a_diamond_gain_by_turbine.py
+++ WES2024/Plot/Fig08a_diamond_gain_by_turbine.py
@@ -29,51 +29,92 @@ def generate(regenerate=False) -> pl.DataFrame:
     return df.filter(pl.col("min_dist") == 6.0)
 
 
-def plot(df_full: pl.DataFrame, df: pl.DataFrame):
-    df_agg = df.group_by("group", "method").agg(pl.col("Cp").mean())
+def aggregate_turbine(df_full: pl.DataFrame) -> tuple[pl.DataFrame, ...]:
+    # select only the first turbine in each group
+    turbines_to_keep = utils.DIAMOND_GROUPS.filter(pl.col("face") == 0)["turbine"]
+    df = df_full.filter(pl.col("turbine").is_in(turbines_to_keep))
+
+    df_agg = df.group_by("group", "method").agg(pl.col("Cp").mean(), pl.col("Ct").mean())
     df_ref = df_agg.filter(pl.col("method") == "NoControl")
 
     df_agg = (
         df_agg.join(df_ref, on="group")
-        .select("group", "method", (100 * (pl.col("Cp") / pl.col("Cp_right") - 1)).alias("Cp"))
+        .select(
+            "group",
+            "method",
+            (100 * (pl.col("Cp") / pl.col("Cp_right") - 1)).alias("Cp"),
+            (100 * (pl.col("Ct") / pl.col("Ct_right") - 1)).alias("Ct"),
+        )
         .filter(pl.col("method") != "NoControl")
         .sort("group")
     )
-    df_farm_agg = df_full.group_by("method").agg(pl.col("Cp").mean())
+    df_farm_agg = df_full.group_by("method").agg(pl.col("Cp").mean(), pl.col("Ct").mean())
     Cp_farm_ref = df_farm_agg.filter(pl.col("method") == "NoControl")["Cp"][0]
+    Ct_farm_ref = df_farm_agg.filter(pl.col("method") == "NoControl")["Ct"][0]
     df_farm_agg = df_farm_agg.with_columns(
-        (100 * (pl.col("Cp") / Cp_farm_ref - 1)).alias("Cp"), pl.lit("farm").alias("group")
+        (100 * (pl.col("Cp") / Cp_farm_ref - 1)).alias("Cp"),
+        (100 * (pl.col("Ct") / Ct_farm_ref - 1)).alias("Ct"),
+        pl.lit("Farm").alias("group"),
     )
-    print(df_farm_agg)
 
-    fig, axes = plt.subplots(1, 2, width_ratios=[1, 1 / 8], figsize=0.8 * np.array([10, 4]))
+    return df_agg, df_farm_agg
+
+
+def plot(df_turb: pl.DataFrame, df_farm: pl.DataFrame):
+    fig, axes = plt.subplots(2, 2, width_ratios=[1, 1 / 8], figsize=0.8 * np.array([10, 4]))
     plt.subplots_adjust(wspace=0.3)
 
     sns.barplot(
-        df_agg,
+        df_turb,
         x="group",
         y="Cp",
         hue="method",
         hue_order=["ThrustControl", "YawControl", "JointControl"],
-        ax=axes[0],
+        palette=utils.controller_colors,
+        ax=axes[0, 0],
         legend=True,
     )
 
     sns.barplot(
-        df_farm_agg,
+        df_farm,
         x="group",
         y="Cp",
         hue="method",
         hue_order=["ThrustControl", "YawControl", "JointControl"],
-        ax=axes[1],
+        palette=utils.controller_colors,
+        ax=axes[0, 1],
         legend=False,
     )
 
-    axes[0].set_ylabel(r"$C_P/C_{P,\mathrm{ref}}$")
-    axes[1].set_ylabel(r"$C_{P,\mathrm{farm}}/C_{P,\mathrm{farm,ref}}$")
+    sns.barplot(
+        df_turb,
+        x="group",
+        y="Ct",
+        hue="method",
+        hue_order=["ThrustControl", "YawControl", "JointControl"],
+        palette=utils.controller_colors,
+        ax=axes[1, 0],
+        legend=False,
+    )
+
+    sns.barplot(
+        df_farm,
+        x="group",
+        y="Ct",
+        hue="method",
+        hue_order=["ThrustControl", "YawControl", "JointControl"],
+        palette=utils.controller_colors,
+        ax=axes[1, 1],
+        legend=False,
+    )
+
+    axes[0, 0].set_ylabel(r"$C_P$ increase (\%)")
+    axes[1, 0].set_ylabel(r"$C_T$ increase (\%)")
+    axes[0, 1].set_ylabel(r"$C_{P,\mathrm{farm}}$ increase (\%)")
+    axes[1, 1].set_ylabel(r"$C_{T,\mathrm{farm}}$ increase (\%)")
 
     sns.move_legend(
-        axes[0],
+        axes[0, 0],
         "lower center",
         bbox_to_anchor=(0.5, 1.02),
         ncol=4,
@@ -81,22 +122,27 @@ def plot(df_full: pl.DataFrame, df: pl.DataFrame):
     )
 
     # Set same y lim on both axes
-    axes[1].set_ylim(*axes[0].get_ylim())
-    axes[1].set_xlabel("")
+    [ax.set_ylim(-5, 15) for ax in axes.ravel()]
 
-    axes[0].set_xlabel("Turbine")
+    # Set axis labels and ticks
+    axes[0, 0].set_xlabel("")
+    axes[0, 1].set_xlabel("")
+    axes[1, 1].set_xlabel("")
+    axes[0, 0].set_xticklabels([])
+    axes[0, 1].set_xticklabels([])
+    axes[1, 1].set_xticklabels([])
+    axes[1, 0].set_xlabel("Turbine")
+    axes[1, 1].set_xlabel("Farm")
+
     plt.savefig(utils.FIGDIR / f"{FILESTEM}.png", dpi=300, bbox_inches="tight")
 
 
 def main():
-    df_quarter = generate(regenerate=False)
-    df_full = utils.fill_in_other_quadrants(df_quarter)
+    df_full = utils.fill_in_other_quadrants(generate(regenerate=False))
 
-    # select only the first turbine in each group
-    turbines_to_keep = utils.DIAMOND_GROUPS.filter(pl.col("face") == 0)["turbine"]
-    df = df_full.filter(pl.col("turbine").is_in(turbines_to_keep))
+    df_turb, df_farm = aggregate_turbine(df_full)
 
-    plot(df_full, df)
+    plot(df_turb, df_farm)
 
 
 if __name__ == "__main__":
