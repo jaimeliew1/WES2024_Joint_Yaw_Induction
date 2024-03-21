fa4ed602b65d93a5287ccb85b349b19aca71cbdc
Jaime Liew <jaimeliew1@gmail.com>
2024-03-21 17:26:33 -0400

    Add JFM style 2 turbine grid search for BEM

---
+++ WES2024/Generate/JFM_layout_gridsearch_BEM.py
@@ -0,0 +1,120 @@
+from itertools import product
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+from scipy.optimize import minimize
+import polars as pl
+import foreach
+from MITRotor.ReferenceTurbines import IEA15MW
+from mitwindfarm.Layout import Layout
+from mitwindfarm.Rotor import BEM
+from mitwindfarm.windfarm import Windfarm
+from WES2024.BEM_gradients import DualBEM
+
+from WES2024 import utils
+from rich import print
+
+FILESTEM = Path(__file__).stem
+
+REGENERATE = True
+PARALLEL = True
+
+PITCH_OPT = -0.018804860075115795
+TSR_OPT = 9.138197665010335
+
+bem = BEM(IEA15MW(), BEM_model=DualBEM)
+windfarm = Windfarm(rotor_model=bem)
+
+layout_single = Layout(np.array([0]), np.array([0.0, 0.0]))
+layout_double = Layout(np.array([0, 8]), np.array([0.0, 0.5]))
+
+# Low res
+yaws = np.arange(0, 45)
+Ctprimes = np.arange(0.5, 3.01, 0.25)
+
+# High res
+yaws = np.arange(0, 45)
+Ctprimes = np.arange(0.5, 3.01, 0.1)
+
+opt_cache = {}
+
+
+def get_opt_solution(yaw: float) -> tuple[float, float, float]:
+    if yaw in opt_cache:
+        return opt_cache[yaw]
+    else:
+
+        def to_opt(x):
+            pitch, tsr = x
+            return -windfarm(layout_single, [(pitch, tsr, np.deg2rad(yaw))]).Cp
+
+        res = minimize(to_opt, (0, 9))
+        pitch, tsr = res.x
+
+        sol = windfarm(layout_single, [(pitch, tsr, np.deg2rad(yaw))]).rotors[0]
+        opt_cache[yaw] = (sol.extra.pitch, sol.extra.tsr, sol.Ctprime)
+
+        return opt_cache[yaw]
+
+
+def calc_BEM_equiv_setpoint(
+    bem: BEM, yaw: float, Ctprime_target: float
+) -> Optional[tuple[float, float]]:
+    # Get optimal set point at given yaw angle
+    pitch_0, tsr_0, Ctprime_0 = get_opt_solution(yaw)
+
+    # if Ctprime_target is too high, return None
+    # if Ctprime_target > Ctprime_0 * 1.2:
+    #     return None
+
+    def to_opt(x):
+        pitch, tsr = x
+        return -windfarm(layout_single, [(pitch, tsr, np.deg2rad(yaw))]).Cp
+
+    def constraint(x):
+        pitch, tsr = x
+        return (
+            windfarm(layout_single, [(pitch, tsr, np.deg2rad(yaw))]).rotors[0].Ctprime
+            - Ctprime_target
+        )
+
+    res = minimize(
+        to_opt,
+        (pitch_0, tsr_0),
+        bounds=[(pitch_0 - np.deg2rad(5), np.deg2rad(20)), (0.5, tsr_0 * 1.2)],
+        constraints=dict(type="eq", fun=constraint),
+    )
+    print(res)
+    if not res.success:
+        return None
+    sol = windfarm(
+        layout_double, [(res.x[0], res.x[1], np.deg2rad(yaw)), (PITCH_OPT, TSR_OPT, 0.0)]
+    )
+    return sol
+
+
+def _generate(x):
+    Ctprime, yaw = x
+    sol = calc_BEM_equiv_setpoint(bem, yaw, Ctprime)
+
+    if sol:
+        return utils.to_polars(sol).with_columns(
+            pl.lit(Ctprime).alias("Ctprime_target"), pl.lit(yaw).alias("yaw_target")
+        )
+    else:
+        return None
+
+
+@utils.cache_polars(utils.CACHEDIR / f"{FILESTEM}.csv")
+def generate(regenerate=False):
+    params = list(product(Ctprimes, yaws))
+
+    dfs = foreach(_generate, params, parallel=PARALLEL)
+    df = pl.concat((df for df in dfs if df is not None))
+    return df
+
+
+if __name__ == "__main__":
+    df = generate(regenerate=REGENERATE)
+    print(df)
---
+++ WES2024/Plot/Fig04a_2_turb_thingy.py
@@ -0,0 +1,73 @@
+"""
+Figure 4:
+2 turbine LES
+
+I am not sure what this figure will do, but it should include results from the
+LES case.
+
+Key points:
+- ??
+
+"""
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+import polars as pl
+
+from WES2024 import utils
+from WES2024.Generate import JFM_layout_gridsearch_BEM
+
+
+FILESTEM = Path(__file__).stem
+
+
+# @utils.cache_polars(utils.CACHEDIR / f"{FILESTEM}.csv")
+def generate(regenerate=False) -> pl.DataFrame:
+    df = JFM_layout_gridsearch_BEM.generate(regenerate=regenerate)
+    return df
+
+
+def plot(df: pl.DataFrame):
+    Ctprime_opt, yaw_opt, _ = (
+        df.group_by("Ctprime_target", "yaw_target")
+        .agg(pl.col("Cp").mean())
+        .filter(Cp=pl.col("Cp").max())
+        .rows()[0]
+    )
+    df_piv = df.pivot(
+        index="Ctprime_target", columns="yaw_target", values="Cp", aggregate_function="mean"
+    )
+    Ctprime = df_piv["Ctprime_target"].to_numpy()
+    yaw = np.array(df_piv.columns[1:], dtype=float)
+    Cp = df_piv.to_numpy()[:, 1:]
+    # Cp[Cp < 0.01] = 0.01
+    # Cp[np.isnan(Cp)] = 0.02
+    fig, ax = plt.subplots(1, 1)
+
+    levels = np.arange(0.3, 0.601, 0.025)
+    CF_Cp = ax.contourf(yaw, Ctprime, Cp, levels=levels, cmap="cividis")
+    CS = ax.contour(yaw, Ctprime, Cp, levels=levels, colors="k", linewidths=0.8)
+    ax.clabel(CS, inline=True, fontsize=10)
+
+    # Add optimal
+    ax.plot(yaw_opt, Ctprime_opt, "*y")
+
+    # Add colorbar
+    cbar = plt.colorbar(CF_Cp, ax=ax, aspect=20)
+    cbar.set_label(label=r"$C_P~$(-)")
+
+    # axis labels
+    ax.set_xlabel(r"yaw (deg)")
+    ax.set_ylabel(r"$C_T'$")
+
+    plt.savefig(utils.FIGDIR / f"{FILESTEM}.png", dpi=300, bbox_inches="tight")
+
+
+def main():
+    df = generate(regenerate=False)
+    plot(df)
+
+
+if __name__ == "__main__":
+    main()
