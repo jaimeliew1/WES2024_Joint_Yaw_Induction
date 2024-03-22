c62b8afda31c9061106f96fc000a306b807d252e
Jaime Liew <jaimeliew1@gmail.com>
2024-03-21 23:25:19 -0400

    Add uncertainty generation and aws launch script

--- scripts/run_aws.sh
+++ scripts/run_aws.sh
@@ -1,6 +1,6 @@
-poetry run ipython WES2024/Plot/Analysis_BEM_windfarm_optima.py  &> log1.txt
-poetry run ipython WES2024/Plot/Fig11_diamond_pitch_tsr_surface.py  &> log2.txt
-aws s3 cp --recursive data s3://jamalama
-aws s3 cp --recursive log1.txt s3://jamalama
-aws s3 cp --recursive log2.txt s3://jamalama
+poetry run ipython WES2024/Generate/diamond_BEM_uncertainty.py  &> log1.txt
+# poetry run ipython WES2024/Plot/Fig11_diamond_pitch_tsr_surface.py  &> log2.txt
+aws s3 cp --recursive data s3://jamalamaa
+aws s3 cp --recursive log1.txt s3://jamalamaa
+# aws s3 cp --recursive log2.txt s3://jamalamaa
 sudo shutdown now -h
