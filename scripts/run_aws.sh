aa1a9054c4511b910ce11ddb42c76b0347a49923
Jaime Liew <jaimeliew1@gmail.com>
2024-06-17 09:52:36 +0200

    update aws script

--- scripts/run_aws.sh
+++ scripts/run_aws.sh
@@ -11,27 +11,27 @@
 # poetry run ipython WES2024/Generate/two_turbine_BEM.py &> log_two_turbine_BEM.txt
 # poetry run ipython WES2024/Generate/two_turbine_BEM_sensitivity.py &> log_two_turbine_BEM_sensitivity.txt
 # poetry run ipython WES2024/Generate/minCt_trajectory.py &> log_minCt_trajectory.txt
-# poetry run ipython WES2024/Generate/diamond_AD.py &> log_diamond_AD.txt
-poetry run ipython WES2024/Generate/diamond_BEM.py &> log_diamond_BEM.txt
-poetry run ipython WES2024/Generate/diamond_BEM_uncertainty.py &> log_diamond_BEM_uncertainty.txt
-poetry run ipython WES2024/Generate/diamond_BEM_uncertainty_postproc.py &> log_diamond_BEM_uncertainty_postproc.txt
-poetry run ipython WES2024/Generate/diamond_AD_sensitivity.py &> log_diamond_AD_sensitivity.txt
+poetry run ipython WES2024/Generate/diamond_AD.py # &> log_diamond_AD.txt
+# poetry run ipython WES2024/Generate/diamond_BEM.py &> log_diamond_BEM.txt
+# poetry run ipython WES2024/Generate/diamond_BEM_uncertainty.py &> log_diamond_BEM_uncertainty.txt
+# poetry run ipython WES2024/Generate/diamond_BEM_uncertainty_postproc.py &> log_diamond_BEM_uncertainty_postproc.txt
+poetry run ipython WES2024/Generate/diamond_AD_sensitivity.py # &> log_diamond_AD_sensitivity.txt
 
 aws s3 cp --recursive data s3://jamalamaa
 
 
-aws s3 cp log__single_turbine_opt.txt s3://jamalamaa
-aws s3 cp log_pitch_tsr_surface.txt s3://jamalamaa
-aws s3 cp log_two_turbine_AD.txt s3://jamalamaa
-aws s3 cp log_two_turbine_AD_sensitivity.txt s3://jamalamaa
-aws s3 cp log_two_turbine_BEM.txt s3://jamalamaa
-aws s3 cp log_two_turbine_BEM_sensitivity.txt s3://jamalamaa
-aws s3 cp log_minCt_trajectory.txt s3://jamalamaa
-aws s3 cp log_diamond_AD.txt s3://jamalamaa
-aws s3 cp log_diamond_BEM.txt s3://jamalamaa
-aws s3 cp log_diamond_BEM_uncertainty.txt s3://jamalamaa
-aws s3 cp log_diamond_BEM_uncertainty_postproc.txt s3://jamalamaa
-aws s3 cp log_diamond_AD_sensitivity.txt s3://jamalamaa
+# aws s3 cp log__single_turbine_opt.txt s3://jamalamaa
+# aws s3 cp log_pitch_tsr_surface.txt s3://jamalamaa
+# aws s3 cp log_two_turbine_AD.txt s3://jamalamaa
+# aws s3 cp log_two_turbine_AD_sensitivity.txt s3://jamalamaa
+# aws s3 cp log_two_turbine_BEM.txt s3://jamalamaa
+# aws s3 cp log_two_turbine_BEM_sensitivity.txt s3://jamalamaa
+# aws s3 cp log_minCt_trajectory.txt s3://jamalamaa
+# aws s3 cp log_diamond_AD.txt s3://jamalamaa
+# aws s3 cp log_diamond_BEM.txt s3://jamalamaa
+# aws s3 cp log_diamond_BEM_uncertainty.txt s3://jamalamaa
+# aws s3 cp log_diamond_BEM_uncertainty_postproc.txt s3://jamalamaa
+# aws s3 cp log_diamond_AD_sensitivity.txt s3://jamalamaa
 
 sudo shutdown now -h
 
