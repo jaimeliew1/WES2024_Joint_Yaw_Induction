a724b5c541b0afd2fd6e46a1c05192021247d97c
Ilan Upfal <103550915+iupfal@users.noreply.github.com>
2025-04-16 22:23:01 -0400

    small edits

--- WES2024/Generate/minCt_trajectory.py
+++ WES2024/Generate/minCt_trajectory.py
@@ -19,7 +19,8 @@ REGENERATE = True
 
 FILESTEM = Path(__file__).stem
 
-YAWS = np.arange(0.0, 50.1, 2.5)
+# YAWS = np.arange(0.0, 50.1, 2.5)
+YAWS = [0.0, 45.0]
 
 rotor = IEA15MW()
 bem = BEM(rotor=rotor, 
@@ -128,7 +129,8 @@ def generate_derate_strat(bem: BEM, yaw: float, N_Cp: int = 10, N_theta: int = 2
     sol_opt: BEMSolution = find_optimal_setpoint(bem, yaw)
     Cp_opt = sol_opt.Cp()
 
-    Cps = np.linspace(0.35 * Cp_opt, Cp_opt - 0.01, N_Cp)
+    # Cps = np.linspace(0.35 * Cp_opt, Cp_opt - 0.01, N_Cp)
+    Cps = np.linspace(0.35 * Cp_opt, Cp_opt, N_Cp, endpoint=False)
 
     trajectory = []
     for Cp in Cps:
@@ -170,7 +172,7 @@ def derate_spline(bem: BEM, yaw: float, N_Cp: int = 10, N_theta: int = 20) -> BS
 
 def _generate(x):
     yaw = x
-    return generate_derate_strat(bem, np.deg2rad(yaw), N_Cp=20).with_columns(yaw=yaw)
+    return generate_derate_strat(bem, np.deg2rad(yaw), N_Cp=40).with_columns(yaw=yaw)
 
 
 @utils.cache_polars(utils.CACHEDIR / f"{FILESTEM}.csv")
--- WES2024/Plot/ForPaper/Fig00_single_turbine_surface.py
+++ WES2024/Plot/ForPaper/Fig00_single_turbine_surface.py
@@ -178,28 +178,49 @@ def plot(df_surface: pl.DataFrame, df_trajectory: pl.DataFrame):
     # Plot zero-yaw trajectory
     for ax in axes.ravel():
         dat = df_trajectory.filter(yaw=0.0)
+        # (strat3,) = ax.plot(
+        #     dat["pitch"],
+        #     dat["tsr"],
+        #     ls="-",
+        #     color="black",
+        #     label=r"$\gamma=0^o$",
+        #     ms=8,
+        #     zorder=10,
+        # )
         (strat3,) = ax.plot(
-            dat["pitch"],
-            dat["tsr"],
-            ls="-",
-            color="black",
-            label=r"$\gamma=0^o$",
-            ms=8,
-            zorder=10,
-        )
+        dat["pitch"],
+        dat["tsr"],
+        ls="",
+        color="black",
+        label=r"$\gamma=0^o$",
+        ms=2,
+        zorder=10,
+        marker="o",
+    )
 
     # Plot 45 degree yaw trajectory
     for ax in axes[:, 1]:
         dat = df_trajectory.filter(yaw=YAW2)
+        # (strat4,) = ax.plot(
+        #     dat["pitch"],
+        #     dat["tsr"],
+        #     ls="-",
+        #     color="tab:orange",
+        #     label=r"$\gamma=45^o$",
+        #     ms=8,
+        #     zorder=10,
+        # )
         (strat4,) = ax.plot(
-            dat["pitch"],
-            dat["tsr"],
-            ls="-",
-            color="tab:orange",
-            label=r"$\gamma=45^o$",
-            ms=8,
-            zorder=10,
-        )
+        dat["pitch"],
+        dat["tsr"],
+        ls="",
+        color="tab:orange",
+        label=r"$\gamma=45^o$",
+        ms=2,
+        zorder=10,
+        # ls = "",
+        marker="o",
+    )
 
     # Plot where the global optimal goes
     dat = df_trajectory.group_by("yaw", maintain_order=True).agg(
