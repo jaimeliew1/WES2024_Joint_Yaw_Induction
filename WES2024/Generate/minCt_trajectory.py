fix isobar issues in minCt calculation

WES2024/Generate/minCt_trajectory.py
@@ -19,8 +19,8 @@ REGENERATE = True
 
 FILESTEM = Path(__file__).stem
 
-# YAWS = np.arange(0.0, 50.1, 2.5)
-YAWS = [0.0, 45.0]
+YAWS = np.arange(0.0, 45.1, 5.0)
+# YAWS = [0.0, 45.0]
 
 rotor = IEA15MW()
 bem = BEM(rotor=rotor, 
@@ -69,7 +69,7 @@ def Cp_isobars(Cp_target: float, bem: BEM, sol_opt: BEMSolution, N_theta: int = 
 
     x0 = np.array([sol_opt.pitch, sol_opt.tsr])
     points = []
-    angles = np.linspace(0.05, 0.7 * np.pi, N_theta)
+    angles = np.linspace(-0.05, 0.7 * np.pi, N_theta)
     for angle in angles:
         dx = np.array([0.1 * np.cos(angle), -3 * np.sin(angle)])
 
@@ -83,7 +83,8 @@ def Cp_isobars(Cp_target: float, bem: BEM, sol_opt: BEMSolution, N_theta: int = 
         if res.converged:
             sol = bem(*(x0 + res.root * dx), sol_opt.yaw)
             points.append((sol.pitch, sol.tsr, sol.Cp(), sol.Ctprime()))
-
+        else:
+            print(res)
     return points
 
 
@@ -130,7 +131,7 @@ def generate_derate_strat(bem: BEM, yaw: float, N_Cp: int = 10, N_theta: int = 2
     Cp_opt = sol_opt.Cp()
 
     # Cps = np.linspace(0.35 * Cp_opt, Cp_opt - 0.01, N_Cp)
-    Cps = np.linspace(0.35 * Cp_opt, Cp_opt, N_Cp, endpoint=False)
+    Cps = np.linspace(0.35 * Cp_opt, 0.98 * Cp_opt, N_Cp, endpoint=False)
 
     trajectory = []
     for Cp in Cps:
@@ -172,7 +173,7 @@ def derate_spline(bem: BEM, yaw: float, N_Cp: int = 10, N_theta: int = 20) -> BS
 
 def _generate(x):
     yaw = x
-    return generate_derate_strat(bem, np.deg2rad(yaw), N_Cp=40).with_columns(yaw=yaw)
+    return generate_derate_strat(bem, np.deg2rad(yaw), N_Cp=30, N_theta=30).with_columns(yaw=yaw)
 
 
 @utils.cache_polars(utils.CACHEDIR / f"{FILESTEM}.csv")