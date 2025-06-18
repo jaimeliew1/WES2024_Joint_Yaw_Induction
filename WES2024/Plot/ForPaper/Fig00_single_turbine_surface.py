abb761534f12eb6e29d286520529a088a43c658b
Kirby <kirby.heck@gmail.com>
2025-06-18 18:20:35 -0400

    Reverted plotting line styles back to paper figure

--- WES2024/Plot/ForPaper/Fig00_single_turbine_surface.py
+++ WES2024/Plot/ForPaper/Fig00_single_turbine_surface.py
@@ -153,7 +153,7 @@ def plot(df_surface: pl.DataFrame, df_trajectory: pl.DataFrame):
             dat["tsr"],
             "*",
             color="black",
-            label=r"$\gamma=0^o$",
+            label=r"$\gamma=0^\circ$",
             ms=8,
             zorder=10,
         )
@@ -170,7 +170,7 @@ def plot(df_surface: pl.DataFrame, df_trajectory: pl.DataFrame):
             dat["tsr"],
             "*",
             color="tab:orange",
-            label=r"$\gamma=45^o$",
+            label=r"$\gamma=45^\circ$",
             ms=8,
             zorder=10,
         )
@@ -178,49 +178,49 @@ def plot(df_surface: pl.DataFrame, df_trajectory: pl.DataFrame):
     # Plot zero-yaw trajectory
     for ax in axes.ravel():
         dat = df_trajectory.filter(yaw=0.0)
-        # (strat3,) = ax.plot(
-        #     dat["pitch"],
-        #     dat["tsr"],
-        #     ls="-",
-        #     color="black",
-        #     label=r"$\gamma=0^o$",
-        #     ms=8,
-        #     zorder=10,
-        # )
         (strat3,) = ax.plot(
-        dat["pitch"],
-        dat["tsr"],
-        ls="",
-        color="black",
-        label=r"$\gamma=0^o$",
-        ms=2,
-        zorder=10,
-        marker="o",
-    )
+            dat["pitch"],
+            dat["tsr"],
+            ls="-",
+            color="black",
+            label=r"$\gamma=0^\circ$",
+            ms=8,
+            zorder=10,
+        )
+    #     (strat3,) = ax.plot(
+    #     dat["pitch"],
+    #     dat["tsr"],
+    #     ls="",
+    #     color="black",
+    #     label=r"$\gamma=0^\circ$",
+    #     ms=2,
+    #     zorder=10,
+    #     marker="o",
+    # )
 
     # Plot 45 degree yaw trajectory
     for ax in axes[:, 1]:
         dat = df_trajectory.filter(yaw=YAW2)
-        # (strat4,) = ax.plot(
-        #     dat["pitch"],
-        #     dat["tsr"],
-        #     ls="-",
-        #     color="tab:orange",
-        #     label=r"$\gamma=45^o$",
-        #     ms=8,
-        #     zorder=10,
-        # )
         (strat4,) = ax.plot(
-        dat["pitch"],
-        dat["tsr"],
-        ls="",
-        color="tab:orange",
-        label=r"$\gamma=45^o$",
-        ms=2,
-        zorder=10,
-        # ls = "",
-        marker="o",
-    )
+            dat["pitch"],
+            dat["tsr"],
+            ls="-",
+            color="tab:orange",
+            label=r"$\gamma=45^\circ$",
+            ms=8,
+            zorder=10,
+        )
+    #     (strat4,) = ax.plot(
+    #     dat["pitch"],
+    #     dat["tsr"],
+    #     ls="",
+    #     color="tab:orange",
+    #     label=r"$\gamma=45^\circ$",
+    #     ms=2,
+    #     zorder=10,
+    #     # ls = "",
+    #     marker="o",
+    # )
 
     # Plot where the global optimal goes
     dat = df_trajectory.group_by("yaw", maintain_order=True).agg(
@@ -254,28 +254,28 @@ def plot(df_surface: pl.DataFrame, df_trajectory: pl.DataFrame):
     axes[0, 0].text(
         0.02,
         1.01,
-        r"a) $C_P$ ($\gamma=" + f"{0}" + "^o$)",
+        r"a) $C_P$ ($\gamma=" + f"{0}" + r"^\circ$)",
         transform=axes[0, 0].transAxes,
         **text_props,
     )
     axes[0, 1].text(
         0.02,
         1.01,
-        r"b) $C_P$ ($\gamma=" + f"{YAW2}" + "^o$)",
+        r"b) $C_P$ ($\gamma=" + f"{YAW2}" + r"^\circ$)",
         transform=axes[0, 1].transAxes,
         **text_props,
     )
     axes[1, 0].text(
         0.02,
         1.01,
-        r"c) $C_T$ ($\gamma=" + f"{0}" + "^o$)",
+        r"c) $C_T$ ($\gamma=" + f"{0}" + r"^\circ$)",
         transform=axes[1, 0].transAxes,
         **text_props,
     )
     axes[1, 1].text(
         0.02,
         1.01,
-        r"d) $C_T$ ($\gamma=" + f"{YAW2}" + "^o$)",
+        r"d) $C_T$ ($\gamma=" + f"{YAW2}" + r"^\circ$)",
         transform=axes[1, 1].transAxes,
         **text_props,
     )
